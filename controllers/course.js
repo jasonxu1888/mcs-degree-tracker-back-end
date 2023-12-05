0d7a00533f49cfdba79e9c7b0db8aa388b5099d3
jasonxu1888 <jxu1888@gmail.com>
2023-12-05 16:18:14 -0600

    finish easier routes

--- controllers/course.js
+++ controllers/course.js
@@ -1,11 +1,46 @@
-const course = require("../models/course.js");
+const courseModel = require("../models/course.js");
+const userModel = require("../models/user.js");
 
-//get '/course/:courseid'
-const getCourse = async (req, res, next) => {};
+const getCourse = async (req, res) => {
+  try {
+    var CRN = req.params.id;
+    console.log(CRN);
+    var course = await courseModel.findOne({ id: CRN });
+    if (course === null) {
+      res.status(404).json({
+        message: "Not Found",
+        data: course,
+      });
+    } else {
+      res.status(200).json({
+        message: "OK",
+        data: course,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      data: error,
+    });
+  }
+};
 
-const getAllCourses = async (req, res, next) => {};
+const getAllCourses = async (req, res) => {
+  try {
+    var courses = await courseModel.find();
+    res.status(200).json({
+      message: "OK",
+      data: courses,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      data: error,
+    });
+  }
+};
 
-const validateCourseSchedule = async (req, res, next) => {};
+const validateCourseSchedule = async (req, res) => {};
 
 //export controller functions
 module.exports = {
